91760593a995ccc9b02411900033e28914ca9c04
Scott Searcy <smsearcy14@outlook.com>
2021-12-09 06:32:14 -0800

    More maintenance
    Renamed folders so they will sort properly the whole month.
    Added some notes about my 2021 approach.
    Updated the Python solution template.

---
+++ 2021/README.md
@@ -0,0 +1,10 @@
+AoC 2021
+========
+
+This is my time doing the Advent of Code.
+Since I'm trying to keep up with the problems over the month of December,
+I'm using Python since it's the language I'm most familiar with.
+
+Note: I'm generally working toward getting the problem solved in the least
+amount of coding time,
+and generally am not going to go back and clean up the code.
--- solution.py
+++ solution.py
@@ -1,38 +1,51 @@
 #!/usr/bin/env python3
 """Template for Advent of Code solution in Python.
 
-Usage: solution.py input.txt 1|2
+Usage: ./solution.py 1|2 FILE
 
 """
 
 from __future__ import annotations
 
 import sys
+import typing as t
+from argparse import ArgumentParser
+from dataclasses import dataclass
+
+verbose = False
 
 
 def main():
-    filename = sys.argv[1]
-    action = sys.argv[2]
+    parser = ArgumentParser()
+    parser.add_argument("part", type=int)
+    parser.add_argument("filename")
+    parser.add_argument("--verbose", "-v", action="store_true")
 
-    # read_file(filename)
+    global verbose
+    args = parser.parse_args()
+    filename = args.filename
+    if args.verbose:
+        verbose = True
 
-    if action == "1":
-        pass
-
-    elif action == "2":
-        pass
-
+    if args.part == 1:
+        part1(filename)
+    elif args.part == 2:
+        part2(filename)
     else:
-        return "Invalid action"
+        return f"Invalid 'part' specified: {args.part}"
+
+
+def part1(filename):
+    pass
+
+
+def part2(filename):
+    pass
 
 
 def read_file(filename):
     with open(filename, "r") as f:
         pass
-
-
-def part1():
-    pass
 
 
 if __name__ == "__main__":
